6a6b8111057aa50c71aea734e9a218279b5d4296
seung_ho_choi.s <74850409+chltmdgh522@users.noreply.github.com>
2023-12-08 20:07:15 +0900

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,12 @@
+
+
 from django.urls import path
-from .views import home, profile, RegisterView
+from .views import home, profile, RegisterView, robby_view
+
 
 urlpatterns = [
-    path('', home, name='users-home'),
+    path('home', home, name='users-home'),
+    path('', robby_view, name='users-robby'),
     path('register/', RegisterView.as_view(), name='users-register'),
     path('profile/', profile, name='users-profile'),
 ]
